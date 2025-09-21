9e04f778bd281959e8e5468a936c8b852060ea9c
kamil <kamil.mrowka@outlook.com.au>
2025-09-21 10:41:41 +0200

    Replace app.json to app.config.js

---
+++ app.config.js
@@ -0,0 +1,53 @@
+export default {
+  expo: {
+    name: "Wiewiorka",
+    slug: "wiewiorka",
+    version: "1.0.2",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.mrek.wiewiorka",
+      googleServicesFile: process.env.GOOGLE_SERVICES_PLIST ?? "./GoogleService-Info.plist",
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false
+      }
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      package: "com.mrek.wiewiorka",
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json"
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      "expo-font",
+      "expo-secure-store",
+      "expo-web-browser",
+      "@react-native-firebase/app",
+      "@react-native-firebase/app-distribution"
+    ],
+    experiments: {
+      typedRoutes: true
+    },
+    extra: {
+      eas: {
+        projectId: "5204d94e-0190-43ea-a9b8-1eb28273cc88"
+      }
+    }
+  }
+};
