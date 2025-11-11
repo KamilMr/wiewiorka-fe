5531a170b9ad6223d8d8e0cc76a43c84a9d49793
kamil <kamil.mrowka@outlook.com.au>
2025-11-11 17:24:20 +0100

    Add changelog constant with version history

---
+++ constants/changelog.ts
@@ -0,0 +1,33 @@
+export const CHANGELOG = `# Dziennik zmian
+
+## [1.0.7] - 2025-01-10
+
+### Dodano
+- Wybór kursu EUR/PLN kupno/sprzedaż
+- Synchronizacja wersji aplikacji z package.json
+
+### Naprawiono
+- Nakładanie się paska wyszukiwania i przycisku filtra
+
+## [1.0.6] - 2025-01-08
+
+### Naprawiono
+- Poprawki stabilności aplikacji
+
+## [1.0.5] - 2025-01-05
+
+### Dodano
+- Przyciski kalkulatora do szybkiego wprowadzania kwot
+- Obsługa filtrowania dat częściowych
+
+### Naprawiono
+- Problem z pozostałą ceną
+- Automatyczne zamykanie szuflady nawigacyjnej
+- Otwieranie tylko gdy wymagane
+
+## [1.0.4] - 2025-01-03
+
+### Naprawiono
+- Usunięcie starej wersji
+- Ogólne poprawki i ulepszenia
+`;
